acc08d016c4909b6c2a1ca95e2c522cd59f6ece2
ZhouYu <1477214558@qq.com>
2021-03-21 20:59:23 +0800

    XX

--- advanced-concepts/src/global_values.rs
+++ advanced-concepts/src/global_values.rs
@@ -7,3 +7,20 @@ mod constants {
         println!("{:?}", HEADER)
     }
 }
+
+#[cfg(test)]
+mod statics {
+    static mut BAZ: u32 = 4;
+    static FOO: u8 = 9;
+
+
+    #[test]
+    fn test() {
+        unsafe {
+            println!("bax is {}", BAZ);
+            BAZ = 42;
+            println!("bax is now {}", BAZ);
+            println!("foo is {}", FOO);
+        }
+    }
+}
